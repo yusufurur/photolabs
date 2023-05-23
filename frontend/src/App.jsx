2998be778437c8ee51ff7c4f30d8a9c1729a3afb
yusufurur <yusufurur@gmail.com>
2023-05-23 01:30:22 -0400

    docs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useApplicationData from "./hooks/useApplicationData";
 import Home from "./components/HomeRoute";
 import "./App.scss";
@@ -10,6 +10,28 @@ import PhotoDetailsModel from "./components/PhotoDetailsModal";
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   const { selectedPhoto, setSelectedPhoto, likes, setLikes, photoLike, setPhotoLike, displayModal, setDisplayModal, openPhotoDetails, closePhotoDetails, handlePhotoLike } = useApplicationData(); 
+
+  // const [photoData, setPhotoData] = useState([]);
+  // const [topicData, setTopicData] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const photos = fetch('./api/photos')
+  //     const topics = fetch('./api/topics')
+  //     const response = await Promise.all([photos, topics]);
+  //     const topicResponse = await response[0].json();
+  //     const photoResponse = await response[1].json();
+  //     setPhotoData(photoResponse);
+  //     setTopicData(topicResponse);
+  //   } 
+  //   fetchData();
+  // }, []);
+
+  // const onLoadTopics = async () => {
+  //   const response = await fetch('./api/topics');
+  //   const data = await response.json();
+  //   setTopicData(data);
+  // }
 
   return (
     <div className="App">
